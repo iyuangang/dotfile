2772e762a8f266326d0fb683ae50ea401fae8636
Tim Yuan <yuangang@me.com>
2019-05-10 13:26:56 +0800

    kernel update script add

---
+++ kernel-update.sh
@@ -0,0 +1,7 @@
+sudo eselect kernel list
+cp /usr/src/linux/.config ~/kernel-config-`uname -r`
+# sudo genkernel --kernel-config=/home/timyuan/kernel-config/kernel-config-`uname -r` --makeopts=-j2 --menuconfig --lvm initramfs all
+# sudo mount | grep efivars
+# sudo mount efivars /sys/firmware/efi/efivars -o remount,rw
+# sudo grub-install --modules="linux lvm" --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub --recheck --debug
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
