ea5b44a09cfb17b78e1b8b5f67e1f3686527abf1
Tim Yuan <yuangang@me.com>
2022-08-05 23:16:07 +0800

    feat: update kernel config command

--- kernel-update.sh
+++ kernel-update.sh
@@ -5,3 +5,4 @@ cp /usr/src/linux/.config ~/kernel-config-`uname -r`
 # sudo mount efivars /sys/firmware/efi/efivars -o remount,rw
 # sudo grub-install --modules="linux lvm" --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub --recheck --debug
 # sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo genkernel --kernel-config=/home/timyuan/kernel-config-`uname -r` --kernel-append-localversion=-timyuan --makeopts=-j4 --lvm all
